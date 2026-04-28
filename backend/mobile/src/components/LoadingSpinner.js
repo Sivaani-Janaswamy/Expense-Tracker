f71bbbc79787cd77829794b49374879e2da61222
Sivaani J <sivaanijanaswamy@gmail.com>
2026-04-28 21:48:26 +0530

    Fix: Move Mobile Folder

--- backend/mobile/src/components/LoadingSpinner.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native-paper';
-
-export default function LoadingSpinner() {
-  return <ActivityIndicator style={{ margin: 16 }} />;
-}
